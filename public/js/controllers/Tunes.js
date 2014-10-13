a1b5ed487be0f82b776e3a83bf418487fc2e5feb
Alex Wilmer <dhazium@gmail.com>
2014-10-12 23:48:43 -0400

    styles

--- public/js/controllers/Tunes.js
+++ public/js/controllers/Tunes.js
@@ -1,11 +1,15 @@
 function Tunes ($scope, $http, $sce) {
 
+	var activeIndex = 0
+
 	$http.get('/api/tunes').success(function(tunes) {
 		if (tunes != null) {
 			for (var i=0;i<tunes.length;i++) {
 				tunes[i].safeEmbedCode = $sce.trustAsHtml(tunes[i].embedCode)
+				tunes[i].active = false
 			}
 			$scope.tunes = tunes
+			$scope.tunes[0].active = true
 			$scope.tune = $scope.tunes[0]
 		} else {
 			$scope.tunes = []
@@ -14,6 +18,9 @@ function Tunes ($scope, $http, $sce) {
 
 	$scope.setTune = function(index) {
 		$scope.tune = $scope.tunes[index]
+		$scope.tunes[activeIndex].active = false
+		$scope.tunes[index].active = true
+		activeIndex = index
 	}
 }
 
